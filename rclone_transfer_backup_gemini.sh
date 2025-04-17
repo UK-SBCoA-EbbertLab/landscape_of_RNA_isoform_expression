b233eeb67444680f7fb950a5bd751fbb824d214a
mpage21 <madelinel.page21@gmail.com>
2025-04-17 13:02:54 -0400

    update

--- rclone_transfer_backup_gemini.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rclone copy --progress --transfers 12 --checkers 12 --copy-links ../landscape_of_RNA_isoform_expression/ gemini3:/mnt/gemini3-4/PROJECTS/landscape_of_RNA_isoform_expression
